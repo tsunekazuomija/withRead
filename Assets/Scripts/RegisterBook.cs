a03445b7db8cd0571cbd1f0b4c24f1cc138b5325
Tsunekazu Omija <mijamijaut@g.ecc.u-tokyo.ac.jp>
2023-11-21 19:45:30 +0900

    #6 able to add new book

--- Assets/Scripts/RegisterBook.cs
+++ Assets/Scripts/RegisterBook.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using System.Linq;
 
 public class RegisterBook : MonoBehaviour
 {
@@ -14,8 +15,21 @@ public class RegisterBook : MonoBehaviour
 
         string title = _inputFieldTitle.text;
         int num_chapter = int.Parse(_inputFieldNumChapter.text);
-        
-    }
 
-    
+        string inputString = Resources.Load<TextAsset>("data").ToString();
+        BookShelf bookShelf = JsonUtility.FromJson<BookShelf>(inputString);
+
+        var newBook = new BookData
+        {
+            id = bookShelf.Book.Length,
+            title = title,
+            author = "",
+            num_chapter = num_chapter,
+            progress = Enumerable.Repeat(0, num_chapter).ToArray()
+        };
+
+        bookShelf.Book = bookShelf.Book.Concat(new BookData[] { newBook }).ToArray();
+        string outputString = JsonUtility.ToJson(bookShelf, true);
+        System.IO.File.WriteAllText(Application.dataPath + "/Resources/data.json", outputString);
+    }
 }
