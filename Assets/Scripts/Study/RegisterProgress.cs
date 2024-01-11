a5056d4d29db7a52c810a647c09282bc55ba3519
Tsunekazu Omija <mijamijaut@g.ecc.u-tokyo.ac.jp>
2024-01-11 18:25:25 +0900

    #25 able to register reading progress

--- Assets/Scripts/Study/RegisterProgress.cs
+++ Assets/Scripts/Study/RegisterProgress.cs
@@ -5,89 +5,116 @@ using System;
 using System.IO;
 using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/// <summary>
+/// progresspopupにまとめてしまうこともできるが、あまりにも長くなりそうなので分離する。
+/// registerボタンに割り当てる。
+/// </summary>
 
 public class RegisterProgress : MonoBehaviour
 {
+    public Slider slider1; // from
+    public Slider slider2; // to
+
     SaveData _saveData;
+    BookInfo[] _book;
 
-    int _siblingsIndex;
 
-    void Start()
+    void Awake()
     {
         string filePath = Application.persistentDataPath + "/UserData/data.json";
         string inputString = File.ReadAllText(filePath);
         _saveData = JsonUtility.FromJson<SaveData>(inputString);
-
+        _book = _saveData.book;
     }
 
     public void OnClickRegisterProgress()
     {
-        string title = transform.parent.parent.Find("TitlePlace").Find("Text").GetComponent<TMPro.TextMeshProUGUI>().text;
-
-        for (int i = 0; i < _saveData.book.Length; i++)
+        int _startPage = (int) slider1.value;
+        int _endPage = (int) slider2.value;
+        if (_endPage < _startPage)
         {
-            if (_saveData.book[i].title == title)
+            Debug.Log("start page num is larger than end page num");
+            return;
+        }
+
+        int _bookId = transform.parent.parent.parent.parent.parent.parent.GetComponent<ProgressPopup>().bookId;
+
+        int targetIndex = 0;
+        for (int i=0; i < _book.Length; ++i)
+        {
+            if (_book[i].id == _bookId)
             {
-                _siblingsIndex = i;
+                targetIndex = i;
                 break;
             }
         }
 
-        int _startPage, _endPage;
-        if (transform.parent.Find("FromPageInput").GetComponent<TMPro.TMP_InputField>().text == "")
-        {
-            return;
-        }
-        else if (transform.parent.Find("ToPageInput").GetComponent<TMPro.TMP_InputField>().text == "")
-        {
-            return;
-        }
+        // Todo: targetIndexが見つからなかった場合の処理
 
-        _startPage = int.Parse(transform.parent.Find("FromPageInput").GetComponent<TMPro.TMP_InputField>().text);
-        _endPage = int.Parse(transform.parent.Find("ToPageInput").GetComponent<TMPro.TMP_InputField>().text);
+        BookInfo targetBook = _book[targetIndex];
 
-        BookInfo book = _saveData.book[_siblingsIndex];
-        int[] progress = book.progress;
-        for (int i = _startPage - 1; i < _endPage; i++)
-        {
-            ++progress[i];
-        }
-        book.progress = progress;
-        _saveData.book[_siblingsIndex] = book;
+        targetBook.progress = GetProgress(targetBook.progress, _startPage, _endPage);
+        targetBook.progress_short = GetProgressShort(targetBook.progress_short, targetBook.progress, targetBook.pages, _startPage, _endPage);
 
-        PageCell progress_short = book.progress_short;
-        int[] page_cnt = progress_short.page_cnt;
-        int[] min_read_times = progress_short.min_read_times;
-        for (int i = (_startPage - 1) / 10 ; i <= _endPage / 10; i++)
-        {
-            page_cnt[i] = 0;
-            min_read_times[i] = progress[i * 10];
-            for (int j = 0; j < 10; j++)
-            {
-                // 途中でページがなくなった場合は終了
-                if (i * 10 + j >= book.pages)
-                {
-                    break;
-                }
-
-                if (progress[i * 10 + j] > 0)
-                {
-                    ++page_cnt[i];
-                }
-                if (progress[i * 10 + j] < min_read_times[i])
-                {
-                    min_read_times[i] = progress[i * 10 + j];
-                }
-            }
-        }
-        progress_short.page_cnt = page_cnt;
-        progress_short.min_read_times = min_read_times;
-        book.progress_short = progress_short;
-        _saveData.book[_siblingsIndex] = book;
+        _book[targetIndex] = targetBook;
+        _saveData.book = _book;
 
         string outputString = JsonUtility.ToJson(_saveData, true);
         File.WriteAllText(Application.persistentDataPath + "/UserData/data.json", outputString);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
+    int[] GetProgress(int[] progress, int _startPage, int _endPage)
+    {
+        for (int i = _startPage - 1; i < _endPage; i++)
+        {
+            ++progress[i];
+        }
+        return progress;
+    }
+
+    PageCell GetProgressShort(PageCell progress_s, int[] progress, int pagenum, int startPage, int endPage)
+    {
+        int[] page_cnt = progress_s.page_cnt;
+        int[] min_read_times = progress_s.min_read_times;
+
+        int startIdx = startPage - 1;
+        int endIdx = endPage - 1;
+
+        for (int i = (startIdx) / 10; i < endIdx / 10 + 1; ++i)
+        {
+            page_cnt[i] = 0;
+            min_read_times[i] = 0;
+            for (int j = 0; j < 10; ++j)
+            {
+                // finish if exceed maximum page
+                if (i * 10 + j > pagenum - 1)
+                {
+                    break;
+                }
+
+                // count studied page
+                if (progress[i * 10 + j] > 0)
+                {
+                    ++page_cnt[i];
+                }
+
+                // get minimum read times
+                if (j == 0)
+                {
+                    min_read_times[i] = progress[i * 10 + j]; // = progress[i * 10]
+                } 
+                else if (progress[i * 10 + j] < min_read_times[i])
+                {
+                    min_read_times[i] = progress[i * 10 + j];
+                }
+            }
+        }
+        progress_s.page_cnt = page_cnt;
+        progress_s.min_read_times = min_read_times;
+        return progress_s;
+    }
 }
+
