0d8f8b69c5b0cc828ac953fce07f11192f52775a
Tsunekazu Omija <mijamijaut@g.ecc.u-tokyo.ac.jp>
2024-02-03 15:43:21 +0900

    #40 drive sliders value that the size relationship is maintained

--- Assets/Scripts/Study/ProgressPopup.cs
+++ Assets/Scripts/Study/ProgressPopup.cs
@@ -25,7 +25,7 @@ public class ProgressPopup : MonoBehaviour
     [SerializeField] private TextMeshProUGUI text1;
     [SerializeField] private TextMeshProUGUI text2;
 
-    void Awake()
+    private void Awake()
     {
         string filePath = Application.persistentDataPath + "/UserData/data.json";
         string inputString = File.ReadAllText(filePath);
@@ -33,7 +33,7 @@ public class ProgressPopup : MonoBehaviour
         _book = _saveData.book;
     }
 
-    void OnEnable()
+    private void OnEnable()
     {
         // Todo: 不正な値に対する処理(bookIdが適切でない時)
         bookId = bookIdHolder.GetId();
@@ -49,29 +49,7 @@ public class ProgressPopup : MonoBehaviour
 
         bookTitle.GetComponent<TextMeshProUGUI>().text = currentBook.title;
 
-        // set maxvalue of slider
-        slider1.maxValue = currentBook.pages;
-        slider2.maxValue = currentBook.pages;
-
-        slider1.onValueChanged.AddListener( (value) => 
-        {
-            text1.text = value.ToString();
-        } );
-        slider2.onValueChanged.AddListener( (value) => 
-        {
-            text2.text = value.ToString();
-        } );
-
-        if (currentBook.last_read < currentBook.pages)
-        {
-            slider1.value = currentBook.last_read + 1;
-            slider2.value = currentBook.last_read + 1;
-        }
-        else
-        {
-            slider1.value = currentBook.pages;
-            slider2.value = currentBook.pages;
-        }
+        SetSliders(slider1, slider2, currentBook, text1, text2);
 
         // display progress
         string progress = "";
@@ -84,9 +62,29 @@ public class ProgressPopup : MonoBehaviour
         bookProgress.GetComponent<TextMeshProUGUI>().text = progress;
     }
 
-    void OnDisable()
+    private void OnDisable()
     {
         slider1.value = currentBook.last_read + 1;
         slider2.value = currentBook.last_read + 1;
+    }
+
+    private void SetSliders(Slider s1, Slider s2, BookInfo book, TextMeshProUGUI t1, TextMeshProUGUI t2)
+    {
+        s1.maxValue = book.pages;
+        s2.maxValue = book.pages;
+
+        s1.onValueChanged.AddListener( (value) => t1.text = value.ToString() );
+        s2.onValueChanged.AddListener( (value) => t2.text = value.ToString() );
+
+        if (book.last_read < book.pages)
+        {
+            s1.value = book.last_read + 1;
+            s2.value = book.last_read + 1;
+        }
+        else
+        {
+            s1.value = book.pages;
+            s2.value = book.pages;
+        }
     }
 }
